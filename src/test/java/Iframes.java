babdf83611acb7c9a3270320da993c669ecd05cc
Chris-Owusu <christopher.owusu@amalitech.com>
2024-06-06 10:13:18 +0000

    Modals

--- src/test/java/Iframes.java
+++ src/test/java/Iframes.java
@@ -1,9 +1,10 @@
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
@@ -17,5 +18,27 @@ public class Iframes {
 
         $(By.xpath("//a[normalize-space()='Iframes']")).click();
         assertTrue(WebDriverRunner.url().contains("iframes/"));
+
+        SelenideElement firstFrame = $("#frame2");
+        SelenideElement secondFrame = $("#frame1");
+
+        // First frame
+        switchTo().frame(firstFrame);
+        $(By.xpath("//button[text()=\"Login\"]")).shouldBe(visible);
+        $(By.xpath("//button[aria-label=\"Close Message\"]")).click();
+        $(By.xpath("//a[@href=\"/search\"]")).click();
+        $(By.xpath("//input[@placeholder=\"Search\"]")).val("Space");
+        $(By.xpath("//span[text()=\"Passport to Space\"]")).should(exist);
+
+        // Switch back to main content page
+        switchTo().defaultContent();
+
+        // Second frame
+        switchTo().frame(secondFrame);
+        $(By.xpath("//a[@href=\"/sponsor\"]")).click();
+        $(By.xpath("//h1[text()=\"Sponsors\"]")).shouldBe(visible).shouldHave(exactText("Sponsors"));
+
+        // Switch back to main content page
+        switchTo().defaultContent();
     }
 }
--- src/test/java/Modals.java
+++ src/test/java/Modals.java
@@ -1,7 +1,9 @@
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.testng.Assert.assertEquals;
@@ -16,5 +18,23 @@ public class Modals {
 
         $(By.xpath("//a[normalize-space()='Modals']")).click();
         assertTrue(WebDriverRunner.url().contains("modals/"));
+
+        SelenideElement simpleModal = $("#simpleModal");
+        SelenideElement formModal = $("#formModal");
+
+        // Simple modal
+        simpleModal.click();
+        $("#pum_popup_title_1318").shouldHave(text("Simple Modal"));
+        $("div[id='popmake-1318'] p").shouldHave(text("Hi, I’m a simple modal."));
+        $(By.xpath("//div[@id='popmake-1318']//button[@aria-label='Close'][normalize-space()='×']")).click();
+
+        //Form Modal
+        formModal.click();
+        $("#pum_popup_title_674").shouldHave(exactText("Modal Containing A Form"));
+        $("#g1051-name").val("Mark");
+        $("#g1051-email").val("mark@aol.com");
+        $("#contact-form-comment-g1051-message").val("Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\n" +
+                "molestiae");
+        $("button[class='pushbutton-wide']").click();
     }
 }

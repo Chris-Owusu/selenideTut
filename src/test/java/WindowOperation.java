de0d68929bdb72e1ff0478db640242e30f942684
Chris-Owusu <christopher.owusu@amalitech.com>
2024-06-06 16:39:44 +0000

    Window Operation

--- src/test/java/WindowOperation.java
+++ src/test/java/WindowOperation.java
@@ -1,9 +1,11 @@
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.*;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
@@ -11,9 +13,57 @@ public class WindowOperation {
     @Test
     public void windowsOperation() {
         open("https://practice-automation.com/");
-        assertEquals(WebDriverRunner.url(), "https://practice-automation.com/");
+        assertEquals(url(), "https://practice-automation.com/");
 
         $(By.xpath("//a[normalize-space()='Window Operations']")).click();
-        assertTrue(WebDriverRunner.url().contains("window-operations/"));
+        assertTrue(url().contains("window-operations/"));
+
+        // Get main window
+        String mainWindow = WebDriverRunner.getWebDriver().getWindowHandle();
+
+        // New Table
+        $("button[onclick='newTab()']").click();
+
+        // switch to new window
+        for (String windowHandle : WebDriverRunner.getWebDriver().getWindowHandles()) {
+            if (!windowHandle.equals(mainWindow)) {
+                WebDriverRunner.getWebDriver().switchTo().window(windowHandle);
+                break;
+            }
+        }
+
+        assertEquals(url(), "https://automatenow.io/");
+
+        // switch back to main window
+        WebDriverRunner.getWebDriver().switchTo().window(mainWindow);
+
+        assertEquals(url(), "https://practice-automation.com/window-operations/");
+
+
+        // Replace Windows
+        $("button[onclick='newWindowSelf()']").click();
+        assertEquals(url(), "https://automatenow.io/");
+
+        Selenide.back();
+
+        assertEquals(url(), "https://practice-automation.com/window-operations/");
+
+
+        // New Window
+        $("button[onclick='newWindow()']").click();
+
+        // switch to new window
+        for (String windowHandle : WebDriverRunner.getWebDriver().getWindowHandles()) {
+            if (!windowHandle.equals(mainWindow)) {
+                WebDriverRunner.getWebDriver().switchTo().window(windowHandle);
+                break;
+            }
+        }
+
+        assertEquals(url(), "https://automatenow.io/");
+
+        WebDriverRunner.getWebDriver().switchTo().window(mainWindow);
+
+        assertEquals(url(), "https://practice-automation.com/window-operations/");
     }
 }

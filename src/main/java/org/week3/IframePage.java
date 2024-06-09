f1b39fd836d6401bb4e321100eaaf45e789a20bc
Chris-Owusu <christopher.owusu@amalitech.com>
2024-06-09 15:53:05 +0000

    iframe page and test

--- src/main/java/org/week3/IframePage.java
+++ src/main/java/org/week3/IframePage.java
@@ -1,2 +1,66 @@
-package org.week3;public class IframePage {
+package org.week3;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.switchTo;
+
+public class IframePage {
+
+    public SelenideElement iframesBtn() {
+        return $(By.xpath("//a[normalize-space()='Iframes']"));
+    }
+
+    SelenideElement firstFrame = $("#frame2");
+    SelenideElement secondFrame = $("#frame1");
+
+    // First frame
+    public SelenideElement switchToFirstFrame() {
+        return (SelenideElement) switchTo().frame(firstFrame);
+    }
+
+    public SelenideElement loginBtnVisible() {
+        return $(By.xpath("//button[text()=\"Login\"]"));
+    }
+
+    public SelenideElement closePopUp() {
+        return $(By.xpath("//button[aria-label=\"Close Message\"]"));
+    }
+
+    public SelenideElement clickSearchBtn() {
+        return $(By.xpath("//a[@href=\"/search\"]"));
+    }
+
+    public SelenideElement makeSearch(String search) {
+        return $(By.xpath("//input[@placeholder=\"Search\"]")).val(search);
+    }
+
+    public SelenideElement assertSearchResult() {
+        return $(By.xpath("//span[text()=\"Passport to Space\"]"));
+    }
+
+    // Switch back to main content page
+    public SelenideElement switchToDefaultContent() {
+        return (SelenideElement) switchTo().defaultContent();
+    }
+
+    // Second frame
+    public SelenideElement switchToSecondFrame() {
+        return (SelenideElement) switchTo().frame(secondFrame);
+    }
+
+    public SelenideElement clickSponsorBtn() {
+        return $(By.xpath("//a[@href=\"/sponsor\"]"));
+    }
+
+    public SelenideElement assertSponsorText() {
+        return $(By.xpath("//h1[text()=\"Sponsors\"]"))
+                .shouldBe(visible)
+                .shouldHave(exactText("Sponsors"));
+    }
+
 }
--- src/test/java/org/week3/IframeTest.java
+++ src/test/java/org/week3/IframeTest.java
@@ -1,2 +1,31 @@
-package org.week3;public class IframeTest {
+package org.week3;
+
+import com.codeborne.selenide.WebDriverRunner;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class IframeTest extends BaseTest {
+
+    IframePage iframePage = new IframePage();
+
+
+    @Test
+    public void iframeTest() {
+        assertEquals(WebDriverRunner.url(), "https://practice-automation.com/");
+        iframePage.iframesBtn().click();
+        assertTrue(WebDriverRunner.url().contains("iframes/"));
+        iframePage.switchToFirstFrame();
+        iframePage.loginBtnVisible();
+        iframePage.closePopUp().click();
+        iframePage.clickSearchBtn().click();
+        iframePage.makeSearch("Space");
+        iframePage.assertSearchResult();
+        iframePage.switchToDefaultContent();
+        iframePage.switchToSecondFrame();
+        iframePage.clickSponsorBtn().click();
+        iframePage.assertSponsorText();
+        iframePage.switchToDefaultContent();
+    }
 }

f5bc7b2fc79043ff8ec9f9686e4b0b16c22be1a3
Chris-Owusu <christopher.owusu@amalitech.com>
2024-06-11 09:43:08 +0000

    tables multiple test

--- src/test/java/org/week3/TablesTest.java
+++ src/test/java/org/week3/TablesTest.java
@@ -1,6 +1,7 @@
 package org.week3;
 
 import com.codeborne.selenide.WebDriverRunner;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -10,15 +11,21 @@ import static org.testng.Assert.assertTrue;
 public class TablesTest extends BaseTest {
     TablesPage tablesPage = new TablesPage();
 
-    @Test
-    public void tablesTest() {
+    @BeforeMethod
+    public void tablesPage() {
         tablesPage.tablesBtn().click();
         assertTrue(WebDriverRunner.url().contains("tables/"));
+    }
 
-        // Simple table
+    // Simple table
+    @Test(priority = 1)
+    public void simpleTablesTest() {
         tablesPage.figures();
+    }
 
-        // Sortable table
+    // Sortable table
+    @Test(priority = 2)
+    public void sortableTablesTest() {
         tablesPage.pagination().selectOption("50");
         assertEquals($("#tablepress-1_info").text(), "Showing 1 to 25 of 25 entries");
         tablesPage.searchTable("Egypt");

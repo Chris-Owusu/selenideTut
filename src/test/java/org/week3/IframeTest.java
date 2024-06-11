0f45bba7be916ab20857dac18cb4376b182c2ca6
Chris-Owusu <christopher.owusu@amalitech.com>
2024-06-11 09:26:40 +0000

    iframe multiple test

--- src/test/java/org/week3/IframeTest.java
+++ src/test/java/org/week3/IframeTest.java
@@ -1,6 +1,8 @@
 package org.week3;
 
 import com.codeborne.selenide.WebDriverRunner;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -10,11 +12,14 @@ public class IframeTest extends BaseTest {
 
     IframePage iframePage = new IframePage();
 
-
-    @Test
-    public void iframeTest() {
+    @BeforeMethod
+    public void iframePage() {
         iframePage.iframesBtn().click();
         assertTrue(WebDriverRunner.url().contains("iframes/"));
+    }
+
+    @Test(priority = 1)
+    public void iframeTest() {
         iframePage.switchToFirstFrame();
         iframePage.loginBtnVisible();
         iframePage.closePopUp().click();
@@ -22,6 +27,10 @@ public class IframeTest extends BaseTest {
         iframePage.makeSearch("Space");
         iframePage.assertSearchResult();
         iframePage.switchToDefaultContent();
+    }
+
+    @Test(priority = 2)
+    public void secondFrame() {
         iframePage.switchToSecondFrame();
         iframePage.clickDocumentationBtn().click();
         iframePage.assertDocsText();
